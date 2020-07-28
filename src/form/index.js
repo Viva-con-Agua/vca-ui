27748bfcf06e89385ad2f394e9e2ae5e459e1c58
Tobi Ka <t.kaestle@vivaconagua.org>
2020-07-28 17:08:55 +0200

    added datepicker

--- src/form/index.js
+++ src/form/index.js
@@ -2,4 +2,5 @@ export { default as VcaForm } from './VcaForm';
 export { default as VcaField } from './VcaField';
 export { default as VcaFieldRow } from './VcaFieldRow';
 export { default as VcaInput } from './VcaInput';
+export { default as VcaInputDate } from './VcaDate';
 export { default as VcaMoneyInput } from './VcaMoneyInput.vue';
