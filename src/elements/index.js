b58c83ba8721247d4bf269df9fe640978fd86ce2
Tobi Ka <t.kaestle@vivaconagua.org>
2020-10-14 15:16:22 +0200

    added share buttons

--- src/elements/index.js
+++ src/elements/index.js
@@ -1,3 +1,4 @@
 export { default as VcaTab } from './VcaTab';
 export { default as VcaTabs } from './VcaTabs';
 export { default as VcaCard } from './VcaCard';
+export { default as VcaShareButtons } from './VcaShareButtons';
