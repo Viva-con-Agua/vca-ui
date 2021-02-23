05acc0b52b9480be608fb0be2823c5ad7b2d7f70
gottemeyer <#Muffin10-10>
2021-02-23 16:07:50 +0100

    added node-sass sass-loader and changed webpack config to auto load scss

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,19 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.scss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'sass-loader'
+        ],
+        loaderOptions: {
+          sass: {
+            additionalData: '@import "@assets/styles/main.scss";',
+          }
+        }
+      }
     ],
   },
 };
