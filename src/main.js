60772360ad1ef60df0f22dda0f94758ce05882ee
Tobi Ka <t.kaestle@vivaconagua.org>
2021-04-29 02:03:38 +0200

    fixed timepicker

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import * as elements from './elements'
 import * as directives from './directives'
 import * as layout from './layout'
 import vSelect from 'vue-select'
+import VueTimepicker from 'vue2-timepicker'
+import 'vue2-timepicker/dist/VueTimepicker.css'
 import 'vue-select/dist/vue-select.css';
 import './assets/main.css'
 
@@ -15,6 +17,7 @@ const allComponents = {
 const VcaUi = Vue => {
   Object.values(allComponents).forEach(Comp => Vue.component(Comp.name, Comp))
   Vue.component('v-select', vSelect)
+  Vue.component('vue-timepicker', VueTimepicker)
   Object.values(directives).forEach(directive =>
     Vue.directive(directive.name, directive)
   )
